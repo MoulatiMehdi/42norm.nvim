aeb5c0c36520b45fcd01cbb9ad857f1bbaa651fc
Mehdi_Moulati <moulati.mehdi@gmail.com>
2024-09-12 21:27:54 +0100

    add format_on_save attribute

--- lua/norminette/init.lua
+++ lua/norminette/init.lua
@@ -1,6 +1,26 @@
 local linter = require("norminette.linter")
 local formatter = require("norminette.formatter")
+local config = require("norminette.config")
 
+local M = {}
+
+M.norminette = linter.check
+M.formatter = formatter.format
+function M.setup(user_config)
+	-- 1. Merge user config with defaults
+	config.setup(user_config)
+
+	-- 2. Use configuration to conditionally set behavior
+	if config.config.format_on_save then
+		-- Set up autocommand for formatOnSave
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*.c,*.h",
+			callback = function()
+				formatter.format()
+			end,
+		})
+	end
+end
 -- Autocommand to attach Norminette to buffers of type .c and .h
 vim.api.nvim_create_autocmd("BufEnter", {
 	pattern = { "*.c", "*.h" },
@@ -14,23 +34,19 @@ vim.api.nvim_create_autocmd("BufEnter", {
 vim.api.nvim_create_autocmd({ "InsertLeave", "BufEnter" }, {
 	pattern = { "*.c", "*.h" },
 	callback = function()
-		linter.norminette()
+		linter.check()
 	end,
 	desc = "Run Norminette on buffer when exiting insert mode",
 })
 
 -- User command to manually run Norminette on the whole file
 vim.api.nvim_create_user_command("Norminette", function()
-	linter.norminette()
+	linter.check()
 end, { desc = "Run Norminette on the whole file" })
 
 -- User command to manually format the current buffer
 vim.api.nvim_create_user_command("Format", function()
-	formatter.formatter()
+	formatter.format()
 end, { desc = "Format the current buffer using 42 norms" })
 
--- Return the norminette and formatter functions for customization
-return {
-	norminette = linter.norminette,
-	formatter = formatter.formatter,
-}
+return M

97b8d44f9f19f6caab4563a9f23f234ccd24854f
Mehdi_Moulati <moulati.mehdi@gmail.com>
2024-09-13 13:04:30 +0100

    update init.lua file of the plugin for testing

--- init.lua
+++ init.lua
@@ -4,10 +4,22 @@ vim.opt.runtimepath:append(".")
 -- Load the Norminette plugin
 local norm = require("norminette")
 
-norm.setup({
-	format_on_save = true,
-})
+norm.setup({})
 -- Example key mapping to run norminette manually
-vim.keymap.set("n", "<F5>", ":Norminette<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<F5>", function()
+	norm.check_norms()
+end, { desc = "Update 42norms diagnostics", noremap = true, silent = true })
 
-vim.keymap.set("n", "<C-f>", ":Format<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-f>", function()
+	norm.format()
+end, { desc = "Format buffer on 42norms", noremap = true, silent = true })
+
+-- User command to manually run Norminette on the whole file
+vim.api.nvim_create_user_command("Norminette", function()
+	norm.check_norms()
+end, { desc = "Update 42norms diagnostics" })
+
+-- User command to manually format the current buffer
+vim.api.nvim_create_user_command("Format", function()
+	norm.format()
+end, { desc = "Format buffer on 42norms" })
